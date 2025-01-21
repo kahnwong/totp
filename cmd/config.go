44cb8fcac1860dcb3d7205f854da0a26bbab5b09
Karn Wong <karn@karnwong.me>
2025-01-21 14:36:05 +0700

    refactor: use cli-base to read config

--- cmd/config.go
+++ cmd/config.go
@@ -1,20 +1,11 @@
 package cmd
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-
-	"github.com/getsops/sops/v3/decrypt"
-
-	"gopkg.in/yaml.v3"
+	cliBase "github.com/kahnwong/cli-base"
 )
 
-// init
-var config = readConfig()
+var config = cliBase.ReadYamlSops[Config]("~/.config/totp/totp.sops.yaml")
 
-// readConfig
 type Config struct {
 	Totp []struct {
 		Org      string `yaml:"org"`
@@ -23,35 +14,4 @@ type Config struct {
 			Token string `yaml:"token"`
 		} `yaml:"accounts"`
 	} `yaml:"totp"`
-}
-
-func readConfig() Config {
-	// check if config exists
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatal(err)
-	}
-	filename := filepath.Join(homeDir, ".config", "totp", "totp.sops.yaml")
-
-	// Check if the file exists
-	_, err = os.Stat(filename)
-	if os.IsNotExist(err) {
-		fmt.Printf("File %s does not exist.\n", filename)
-		os.Exit(1)
-	}
-
-	// parse config
-	var config Config
-
-	data, err := decrypt.File(filename, "yaml")
-	if err != nil {
-		fmt.Println(fmt.Printf("Failed to decrypt: %v", err))
-	}
-
-	err = yaml.Unmarshal(data, &config)
-	if err != nil {
-		fmt.Printf("error: %v\n", err)
-	}
-
-	return config
 }
