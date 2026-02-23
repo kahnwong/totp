e29a29ae4185d2d342fd1e0f60d06deaed258012
Karn Wong <karn@karnwong.me>
2026-02-23 14:06:56 +0700

    chore: bump deps

--- cmd/config.go
+++ cmd/config.go
@@ -1,10 +1,20 @@
 package cmd
 
 import (
+	"log"
+
 	cliBase "github.com/kahnwong/cli-base-sops"
 )
 
-var config = cliBase.ReadYamlSops[Config]("~/.config/totp/totp.sops.yaml")
+var config *Config
+
+func init() {
+	var err error
+	config, err = cliBase.ReadYamlSops[Config]("~/.config/totp/totp.sops.yaml")
+	if err != nil {
+		log.Fatalf("Failed to read config: %v", err)
+	}
+}
 
 type Config struct {
 	Totp []struct {
